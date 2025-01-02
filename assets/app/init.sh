a86f9263f08b18b48e33cff9f3436e2b2cf8f2f0
developerC76 <jsb78241@gmail.com>
2025-01-01 21:16:20 -0500

    added the fact that its for linux first (prob flatpak) into the html webpage

--- assets/app/init.sh
+++ assets/app/init.sh
@@ -1,1 +1,10 @@
 #! /usr/bin/bash
+echo "Exit the app using Ctrl+C if you haven't moved your mess to the Downloads Folder in your user's home (/home/username/Downloads)"
+
+echo "Username: "
+read username
+cd /home/$username/Downloads
+
+echo "Cleaning downloads"
+chmod +x app.py
+./app.py
